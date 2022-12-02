da121ac57885c6750d030d4c199de9aad2065029
Mario <mariopolonia0@gmail.com>
2022-12-01 22:58:49 -0400

    iniciando api con kotlin y spring

---
+++ src/main/kotlin/com/duramas/duramasApi/DuramasApiApplication.kt
@@ -0,0 +1,11 @@
+package com.duramas.duramasApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DuramasApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DuramasApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/duramas/duramasApi/DuramasApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.duramas.duramasApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DuramasApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
